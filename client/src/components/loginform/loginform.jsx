adc22800d31f9a0683bc6ddb233bd3172f93f98a
Arturas Dirvonskis <Arturas.Dirvonskis@probanx.com>
2022-02-26 13:03:59 +0200

    Add some updates in loginform.jsx - not finished yet

--- client/src/components/loginform/loginform.jsx
+++ client/src/components/loginform/loginform.jsx
@@ -6,34 +6,48 @@ import {useMutation} from "@apollo/client";
 import {LOGIN_USER} from "../../mutation/user";
 
 function LoginForm() {
+
+    const createFormData = ( clientid, email, name ) => {
+        return { clientid: clientid, email: email, name: name };
+    }
+
+    const [formData, setFormData]       = useState(createFormData( 'ju16a6m81mhid5ue1z3v2g0uh', 'your@email.address', 'Your Name' ));
+
     const [submitted, setSubmitted]     = useState(false);
 
-    const [clientid, setClientid] = useState( 'ju16a6m81mhid5ue1z3v2g0uh' );
-    const [email, setEmail] = useState( 'your@email.address' );
-    const [name, setName] = useState( 'Your Name' );
-
-    const [success, setSuccess]         = useState( false );
-    // const [error, setError]             = useState( null );
-
+    const [loginSuccess, setLoginSuccess]                   = useState( false );
+    const [unsuccessfulLoginText, setUnsuccessfulLoginText]         = useState( null );
 
     const [loginUser, { data, loading, error }]                 = useMutation(LOGIN_USER);
 
     const handleChange = (event) => {
-
+        const fData = createFormData( formData.name, formData.email, formData.name );
+        fData[event.target.name] = event.target.value;
+        setFormData( fData );
     }
 
     const handleSubmit = (event) => {
-        event.preventDefault()
+        event.preventDefault();
+        setLoginSuccess( false );
+        setUnsuccessfulLoginText( null );
         loginUser({
             variables: {
                 input: {
-                    client_id: clientid, email: email, name: name
+                    client_id: formData.clientid, email: formData.email, name: formData.name
                 }
             }
         }).then(({data}) => {
-            console.log(data)
+            console.log(data);
+            const status = data.status;
+            setLoginSuccess( true );
+            setTimeout( () => window.location = '/posts', 3000 );
         }).catch( (e) => {
             console.log( e );
+            const status = data.status;
+            if ( status !== 200 )
+                setUnsuccessfulLoginText( data.statusText );
+            else
+                setUnsuccessfulLoginText( e.message );
         })
         setSubmitted( true );
         setTimeout( ()=> setSubmitted( false ), 5000 );
@@ -47,7 +61,7 @@ function LoginForm() {
                 label="ClientId"
                 onChange={handleChange}
                 name="clientid"
-                value={clientid}
+                value={formData.clientid}
                 validators={['required']}
                 errorMessages={['this field is required']}
             />
@@ -56,32 +70,32 @@ function LoginForm() {
                 label="Email"
                 onChange={handleChange}
                 name="email"
-                value={email}
+                value={formData.email}
                 validators={['required', 'isEmail']}
                 errorMessages={['this field is required', 'email is not valid']}
             />
+            <br />
             <TextValidator
-                label="ClientId"
+                label="Name"
                 onChange={handleChange}
                 name="name"
-                value={name}
+                value={formData.name}
                 validators={['required']}
                 errorMessages={['this field is required']}
             />
             <br />
-            { !submitted && <Button
+            <Button
                 color="primary"
                 variant="contained"
                 type="submit"
-                disabled={submitted}
+                disabled={loading}
             >
                 Submit
-            </Button> }
-            { submitted && <CircularProgress /> }
+            </Button>
+            { loading && <div><br/><CircularProgress /></div> }
             <br/>
-            <br/>
-            { success && <Alert severity="success">Your consent was submitted - thanks!</Alert> }
-            { ( error !== null ) && <Alert severity="error">{error}</Alert> }
+            { loginSuccess && <Alert severity="success">Login went well - you will be redirect soon to the user page</Alert> }
+            { ( unsuccessfulLoginText ) && <Alert severity="error">{unsuccessfulLoginText}</Alert> }
         </ValidatorForm>
     )
 }
