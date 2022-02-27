4c7e039afcae1084ceae83747c756c35e5b98690
Arturas Dirvonskis <Arturas.Dirvonskis@probanx.com>
2022-02-27 18:49:29 +0200

    Cleanup

--- client/src/components/statistics/useD3.jsx
+++ client/src/components/statistics/useD3.jsx
@@ -7,13 +7,9 @@ export const useD3 = (renderChartFn, dependencies) => {
 
     const ref = React.useRef();
 
-
     React.useEffect(() => {
-
         renderChartFn(d3.select(ref.current));
-
         return () => {};
-
     }, dependencies);
 
     return ref;
