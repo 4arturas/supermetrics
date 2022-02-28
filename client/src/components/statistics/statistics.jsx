be5800bb065e1457c4d4e0cc6bc7906fcb12939b
Arturas Dirvonskis <Arturas.Dirvonskis@probanx.com>
2022-02-28 14:10:07 +0200

    Some updates

--- client/src/components/statistics/statistics.jsx
+++ client/src/components/statistics/statistics.jsx
@@ -2,8 +2,7 @@ import React, {useContext, useEffect, useState} from "react";
 import {LoginContext, PostsContext} from "../../context/context";
 import Unauthorized from "../loginform/unauthorized";
 import BarChart from './BarChart';
-import moment from "moment";
-import {Card, CardContent, CircularProgress, Typography} from "@mui/material";
+import {Card, CardContent, Typography} from "@mui/material";
 import {
     AVERAGE_CHARACTERS_LENGTH_OF_POSTS_PER_MONTH, AVERAGE_NUMBER_OF_POSTS_PER_USER_PER_MONTH,
     LONGEST_POST_BY_CHARACTER_LENGTH_PER_MMONTH, TOTAL_LONGEST_POST_BY_CHARACTER_LENGTH_PER_MONTH
@@ -18,8 +17,6 @@ function Statistics() {
     const [longestPost, setLongestPost] = useState([]);
     const [longestPost2, setLongestPost2] = useState([]);
     const [averageNumberOfPostsPerUserPerMonth, setAverageNumberOfPostsPerUserPerMonth] = useState([]);
-    let messageLengthPosts = [];
-
 
 
     const {data: averageDataQQL, loading: loadingAverageDataQQL, errorAverageDataQQL} = useQuery(AVERAGE_CHARACTERS_LENGTH_OF_POSTS_PER_MONTH)
@@ -29,16 +26,16 @@ function Statistics() {
     useEffect(() => {
 
         if (!loadingAverageDataQQL)
-            setAverage(averageDataQQL.averageCharactersLengthOfPostsPerMonth.flatMap( d => { return { xxx: d.month, yyy: d.averageCharacterLength } } ) );
+            setAverage(averageDataQQL.averageCharactersLengthOfPostsPerMonth);
 
         if (!loadingLongestPost1QQL)
-            setLongestPost(longestPost1DataQQL.longestPostByCharacterLengthPerMonth.flatMap( d => { return { xxx: d.month, yyy: d.longestMessage } } ) );
+            setLongestPost(longestPost1DataQQL.longestPostByCharacterLengthPerMonth);
 
         if (!loadingLongestPost2QQL)
-            setLongestPost2(longestPost2DataQQL.totalPostsSplitByWeekNumber.flatMap( d => { return { xxx: d.week, yyy: d.messagesCount } } ) );
+            setLongestPost2(longestPost2DataQQL.totalPostsSplitByWeekNumber);
 
         if (!averageNumberOfPostsPerUserPerMonthLoading)
-            setAverageNumberOfPostsPerUserPerMonth( averageNumberOfPostsPerUserPerMonthData.averageNumberOfPostsPerUserPerMonth.flatMap( d => { return { xxx: d.from_id, yyy: (d.averagePerMonth) } } ) );
+            setAverageNumberOfPostsPerUserPerMonth( averageNumberOfPostsPerUserPerMonthData.averageNumberOfPostsPerUserPerMonth);
 
     }, [averageDataQQL, longestPost1DataQQL, longestPost2DataQQL, averageNumberOfPostsPerUserPerMonthData])
 
@@ -56,25 +53,25 @@ function Statistics() {
                 <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
                     Average character length of posts per month
                 </Typography>
-                <BarChart data={average}/>
+                <BarChart data={ average.flatMap( d => { return { xxx: d.month, yyy: d.averageCharacterLength } } )}/>
             </CardContent>
             <CardContent>
                 <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
                     Longest post by character length per month
                 </Typography>
-                <BarChart data={longestPost}/>
+                <BarChart data={longestPost.flatMap( d => { return { xxx: d.month, yyy: d.longestMessage } } )}/>
             </CardContent>
             <CardContent>
                 <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
                     Total posts split by week number
                 </Typography>
-                <BarChart data={longestPost2}/>
+                <BarChart data={longestPost2.flatMap( d => { return { xxx: d.week, yyy: d.messagesCount } } )}/>
             </CardContent>
             <CardContent>
                 <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
                     Average number of posts per user per month
                 </Typography>
-                <BarChart data={averageNumberOfPostsPerUserPerMonth}/>
+                <BarChart data={averageNumberOfPostsPerUserPerMonth.flatMap( d => { return { xxx: d.from_id, yyy: (d.averagePerMonth) } } )}/>
             </CardContent>
         </Card>
 
