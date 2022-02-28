09fd74aa1aae55271a59554e6bf0efe9b53c46b5
Arturas Dirvonskis <Arturas.Dirvonskis@probanx.com>
2022-02-28 11:09:14 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const fs                = require('fs');
 const statistics        = require("./statistics");
 const supermetricsAPI   = require("./supermetricsAPI");
+const alasql = require("alasql");
+const moment = require("moment");
 
 const client_id = 'ju16a6m81mhid5ue1z3v2g0uh';
 const email = 'your@email.address';
@@ -21,7 +23,7 @@ async function saveSupermetricsDataToJSONFile() {
     const posts = [];
     for ( let i = 1; i <= 10; i++ )
     {
-        const supermetricsData = await supermetricsAPI.getDataFromSupermetricsAPI( sl_token, 0 );
+        const supermetricsData = await supermetricsAPI.getDataFromSupermetricsAPI( sl_token, i );
         const p = supermetricsData.data.posts;
         posts.push( ...p );
     }
@@ -41,33 +43,48 @@ function supermetricsTaskImplementation() {
     const postsRawdata = fs.readFileSync(jsonFilePath);
     const posts = JSON.parse(postsRawdata);
 
+
+
+    // return;
+
     // TASK
     // Show stats on the following:
 
     // a. - Average character length of posts per month
     console.log('#########################################################################');
     const average = statistics.averageCharactersLengthOfPostsPerMonth( posts );
-    console.log( 'Average character length of posts per month', average );
+    console.log( 'Average character length of posts per month' );
+    console.log( average );
+    const averageSQL = statistics.averageCharactersLengthOfPostsPerMonthSQL( posts );
+    console.log( 'Average character length of posts per month SQL' );
+    console.log( averageSQL );
 
     // b. - Longest post by character length per month
     console.log('#########################################################################');
     const longestMessage = statistics.longestPostByCharacterLengthPerMonth( posts );
-    console.log( 'Longest post by character length per month', longestMessage );
+    console.log( 'Longest post by character length per month' );
+    console.log( longestMessage );
+    const longestMessageSQL = statistics.longestPostByCharacterLengthPerMonthSQL( posts );
+    console.log( 'Longest post by character length per month SQL' );
+    console.log( longestMessageSQL );
 
     // c. - Total posts split by week number
     console.log('#########################################################################');
     console.log( 'Total posts split by week number' );
     const weeksHashTable = statistics.totalPostsSplitByWeekNumber( posts );
     console.log( weeksHashTable );
+    console.log( 'Total posts split by week number SQL' );
+    const weeksHashTableSQL = statistics.totalPostsSplitByWeekNumberSQL( posts );
+    console.log( weeksHashTableSQL );
 
     // d. - Average number of posts per user per month
     console.log('#########################################################################');
     console.log( 'Average number of posts per user per month' );
     const average2 = statistics.averageNumberOfPostsPerUserPerMonth( posts );
-    const users = Object.keys( average2 );
-    users.map( user => {
-        console.log( 'user', user, 'posted in average ' + average2[user] + ' posts per month' );
-    } );
+    console.log( average2 );
+    const average2SQL = statistics.averageNumberOfPostsPerUserPerMonthSQL( posts );
+    console.log( 'Average number of posts per user per month SQL' );
+    console.log( average2SQL );
 }
 supermetricsTaskImplementation();
 
