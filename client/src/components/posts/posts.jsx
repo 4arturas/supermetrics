54281697073eb4178c507cd87bcc48e53ec1f324
Arturas Dirvonskis <Arturas.Dirvonskis@probanx.com>
2022-03-01 12:13:35 +0200

    Make DataFetcher.jsx separate component

--- client/src/components/posts/posts.jsx
+++ client/src/components/posts/posts.jsx
@@ -1,86 +1,24 @@
 import {useContext, useState} from "react";
 import {LoginContext, PostsContext} from "../../context/context";
 import Unauthorized from "../loginform/unauthorized";
-import Button from "@mui/material/Button";
-import {gql, useLazyQuery} from "@apollo/client";
+
 import {FETCH_SUPERMETRICS_POSTS, GENERATE_RANDOM_POSTS} from "../../query/user";
 import EnhancedTable from "./EnhancedTable";
-import { styled } from '@mui/material/styles';
-import MuiGrid from '@mui/material/Grid';
-import Divider from '@mui/material/Divider';
-import {CircularProgress} from "@mui/material";
+import DataFetcher from "./DataFetcher";
 
-const Grid = styled(MuiGrid)(({ theme }) => ({
-    width: '100%',
-    ...theme.typography.body2,
-    '& [role="separator"]': {
-        margin: theme.spacing(0, 2),
-    },
-}));
 
 function Posts() {
 
     const {sltoken}                     = useContext(LoginContext);
-    const {posts, setPosts}                    = useContext(PostsContext);
-    const [data, setData]               = useState(null);
-    const [loadingData, setLoadingData] = useState( false );
-
-
-    const [fetchSupermetrics, { called: supermetricsCalled, loading: supermetricsLoading, data: supermetricsData }] =
-        useLazyQuery( FETCH_SUPERMETRICS_POSTS, { variables: { sl_token: sltoken }, onCompleted: supermetricsPosts => { setPosts( supermetricsPosts.fetchSupermetricsPosts ); setLoadingData( false ); } } );
-
-    const [randomPosts, { called: randomCalled, loading: randomLoading, data: randomData }] = useLazyQuery( GENERATE_RANDOM_POSTS,
-        { onCompleted: randomPosts => { setPosts( randomPosts.generateRandomPosts ); setLoadingData( false ); } } );
-
-    const handleSupermetricsPosts = (event) => {
-        event.preventDefault();
-        setLoadingData(true);
-        setData( null );
-        setPosts( null );
-        fetchSupermetrics();
-    }
-
-    const handleRandomPosts = (event) => {
-        event.preventDefault();
-        setLoadingData( true );
-        setData( null );
-        setPosts( null );
-        randomPosts();
-    }
+    const {posts}                    = useContext(PostsContext);
 
     if ( !sltoken )
         return <Unauthorized/>
 
     return <div>
-            <h3>Posts</h3>
-            <Grid container>
-                <Grid item xs>
-                    <div>
-                        <Button
-                            color="primary"
-                            variant="contained"
-                            type="submit"
-                            onClick={handleSupermetricsPosts} disabled={loadingData}>Fetch</Button> Supermetrics data
-                    </div>
-                </Grid>
-                <Divider orientation="vertical" flexItem>
-                </Divider>
-                <Grid item xs>
-                    <div style={{textAlign:"right"}}>
-                        <Button
-                            color="primary"
-                            variant="contained"
-                            type="submit"
-                            onClick={handleRandomPosts} disabled={loadingData}>Generate</Button> random data
-                    </div>
-                </Grid>
-                <Grid item xs={12}><br/>
-                    { loadingData && <CircularProgress color="primary" /> }
-                    { posts && <EnhancedTable rows={posts}/> }
-                </Grid>
-            </Grid>
-                {/*{data && data.map((c, i) => <div key={i}>{c.id} - {c.from_name}</div>)}*/}
-            </div>
+        <div><DataFetcher/></div>
+        { posts && <EnhancedTable rows={posts}/> }
+    </div>
 }
 
 export default Posts;
