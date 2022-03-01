f79544a02b3cd88e42184238247874b06777b97e
Arturas Dirvonskis <Arturas.Dirvonskis@probanx.com>
2022-03-01 12:18:32 +0200

    Remove unused stuff

--- client/src/components/posts/posts.jsx
+++ client/src/components/posts/posts.jsx
@@ -1,16 +1,14 @@
-import {useContext, useState} from "react";
+import {useContext} from "react";
 import {LoginContext, PostsContext} from "../../context/context";
 import Unauthorized from "../loginform/unauthorized";
-
-import {FETCH_SUPERMETRICS_POSTS, GENERATE_RANDOM_POSTS} from "../../query/user";
 import EnhancedTable from "./EnhancedTable";
 import DataFetcher from "./DataFetcher";
 
 
 function Posts() {
 
-    const {sltoken}                     = useContext(LoginContext);
-    const {posts}                    = useContext(PostsContext);
+    const {sltoken} = useContext(LoginContext);
+    const {posts}   = useContext(PostsContext);
 
     if ( !sltoken )
         return <Unauthorized/>
