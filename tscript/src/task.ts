1dbff94571411f72a91ec3f279e3586e67fa6ea0
Arturas Dirvonskis <Arturas.Dirvonskis@probanx.com>
2022-03-02 14:41:15 +0200

    Prettify

--- tscript/src/task.ts
+++ tscript/src/task.ts
@@ -29,24 +29,24 @@ async function task() {
     console.log('#########################################################################');
     console.log( 'Average character length of posts per month' );
     const averageCharLengthOfPostsPerMonth: Array<AverageCharLengthOfPostsPerMonth>  = averageCharactersLengthOfPostsPerMonth( posts );
-    averageCharLengthOfPostsPerMonth.forEach( a => console.log( 'month:', a.month, 'averageCharacterLength:', a.averageCharacterLength ) );
+    averageCharLengthOfPostsPerMonth.forEach( a => console.log( `{ month: "${a.month}", averageCharacterLength: ${a.averageCharacterLength} }`) );
 
     // b. - Longest post by character length per month
     console.log('#########################################################################');
     console.log( 'Longest post by character length per month' );
     const longestPostByCharLenPerMonth : Array<LongestPostByCharacterLengthPerMonth> = longestPostByCharacterLengthPerMonth( posts );
-    longestPostByCharLenPerMonth.forEach( l => console.log( 'month:', l.month, 'longestMessage:', l.longestMessage ) );
+    longestPostByCharLenPerMonth.forEach( l => console.log( `{ month: "${l.month}", longestMessage: ${l.longestMessage} }` ) );
 
     // c. - Total posts split by week number
     console.log('#########################################################################');
     console.log( 'Total posts split by week number' );
     const totalPostsSplitByWeekNr : Array<TotalPostsSplitByWeekNumber> = totalPostsSplitByWeekNumber( posts );
-    totalPostsSplitByWeekNr.forEach( t => console.log( 'week:', t.week, 'messagesCount:', t.messagesCount ) )
+    totalPostsSplitByWeekNr.forEach( t => console.log( `{ week: "${t.week}", messagesCount: ${t.messagesCount} }` ) )
 
     // d. - Average number of posts per user per month
     console.log('#########################################################################');
     console.log( 'Average number of posts per user per month' );
     const averageNrOfPostsPerUserPerMonth: Array<AverageNumberOfPostsPerUserPerMonth> = averageNumberOfPostsPerUserPerMonth( posts );
-    averageNrOfPostsPerUserPerMonth.forEach( a => console.log( 'from_id:', a.from_id, 'averagePerMonth:', a.averagePerMonth ) );
+    averageNrOfPostsPerUserPerMonth.forEach( a => console.log( `{ from_id: "${a.from_id}", averagePerMonth: ${a.averagePerMonth} }` ) );
 }
 task();
